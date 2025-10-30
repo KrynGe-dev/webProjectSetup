217d770fb1c5598f0149fed88fd78361f8753d6c
KrynGe-dev <keryangrnm.dev@gmail.com>
2025-10-30 16:30:16 +0100

    Desormais le path de l'utilisateur sera compris en relatif depuis le repertoire parent à notre logiciel.

--- bin/createProject.sh
+++ bin/createProject.sh
@@ -17,60 +17,60 @@ read projectName ;
 
 #Création du parent dans le chemin de direction spécifié par l'utilisateur. Création du premier dossier parent en réutilisant le nom de projet. La commande suivante suffirait, nous décomposons pour plus de lisibilité sur les étapes.
 
-mkdir -pv "$PWD/$filePath/$projectName" ;
+mkdir -pv "$PWD/../www/$filePath/$projectName" ;
 #Logique de création de l'arborescence des DOSSIERS du site de premier niveau.
 
 #racine
-mkdir -pv "$PWD/$filePath/$projectName"/{.well-know,assets,img,pages,scss,docs} ;
+mkdir -pv "$PWD/../www/$filePath/$projectName"/{.well-know,assets,img,pages,scss,docs} ;
 
 #Logique de création de l'arborescence des FICHIERS  du site de premier niveau.
 
 #racine
-touch "$PWD/$filePath/$projectName"/{.browerserlistrc,403.html,404.html,LICENSE,README.md,favicon.png,index.html,robots.txt,sitemap.xml,.editorconfig};
+touch "$PWD/../www/$filePath/$projectName"/{.browerserlistrc,403.html,404.html,LICENSE,README.md,favicon.png,index.html,robots.txt,sitemap.xml,.editorconfig};
 
 #Logique de création de l'arborescence des DOSSIERS de second niveau.
 
 #assets
-mkdir -pv "$PWD/$filePath/$projectName/assets"/{css,fonts,js} ;
+mkdir -pv "$PWD/../www/$filePath/$projectName/assets"/{css,fonts,js} ;
 #img
-mkdir -pv "$PWD/$filePath/$projectName/img/icons/" ;
+mkdir -pv "$PWD/../www/$filePath/$projectName/img/icons/" ;
 #scss
-mkdir -pv "$PWD/$filePath/$projectName/scss"/{abstracts,base,components,layouts,pages,utilities} ;
+mkdir -pv "$PWD/../www/$filePath/$projectName/scss"/{abstracts,base,components,layouts,pages,utilities} ;
 #docs
-mkdir -pv "$PWD/$filePath/$projectName/docs"/{architecture,diagrams,security} ;
+mkdir -pv "$PWD/../www/$filePath/$projectName/docs"/{architecture,diagrams,security} ;
 
 #Logique de création de l'arborescence des FICHIERS de second niveau.
 
 #.well-known
-touch "$PWD/$filePath/$projectName/.wellknown/security.txt" ;
+touch "$PWD/../www/$filePath/$projectName/.wellknown/security.txt" ;
 #pages
-touch "$PWD/$filePath/$projectName/pages"/{about.html,article-details.html,blog.html,contact.html,cookies-policy.html,faq.html,legal-notice.html,portfolio.html,privacy-policy.html,project-details.html,search-results.html,service-details.html,service.html,team.html,terms-conditions.html,testimonials.html};
+touch "$PWD/../www/$filePath/$projectName/pages"/{about.html,article-details.html,blog.html,contact.html,cookies-policy.html,faq.html,legal-notice.html,portfolio.html,privacy-policy.html,project-details.html,search-results.html,service-details.html,service.html,team.html,terms-conditions.html,testimonials.html};
 #scss
-touch "$PWD/$filePath/$projectName/scss/style.css" ;
+touch "$PWD/../www/$filePath/$projectName/scss/style.css" ;
 #docs
-touch "$PWD/$filePath/$projectName/docs"/{workflows.md,README.md} ;
+touch "$PWD/../www/$filePath/$projectName/docs"/{workflows.md,README.md} ;
 
 #Logique de création de l'arborescence des FICHIERS de troisième niveau.
 
 #assets/css
-touch "$PWD/$filePath/$projectName/assets/css"/{syle.css,style.css.map} ;
+touch "$PWD/../www/$filePath/$projectName/assets/css"/{syle.css,style.css.map} ;
 #assets/fonts
-touch "$PWD/$filePath/$projectName/assets/fonts"/{NotoSans-VariableFont.woff2,OFL.txt,README.md} ;
+touch "$PWD/../www/$filePath/$projectName/assets/fonts"/{NotoSans-VariableFont.woff2,OFL.txt,README.md} ;
 #assets/js
-touch "$PWD/$filePath/$projectName/assets/fonts/app.js" ;
+touch "$PWD/../www/$filePath/$projectName/assets/fonts/app.js" ;
 
 #scss/abstracts
-touch "$PWD/$filePath/$projectName/scss/abstracts"/{_functions.scss,_index.scss,_mixins.scss,_variables.scss} ;
+touch "$PWD/../www/$filePath/$projectName/scss/abstracts"/{_functions.scss,_index.scss,_mixins.scss,_variables.scss} ;
 #scss/base
-touch "$PWD/$filePath/$projectName/scss/base"/{_fonts.scss,_global.scss,_index.scss,_reset.scss,_root.scss,_typography.scss} ;
+touch "$PWD/../www/$filePath/$projectName/scss/base"/{_fonts.scss,_global.scss,_index.scss,_reset.scss,_root.scss,_typography.scss} ;
 #scss/components
-touch "$PWD/$filePath/$projectName/scss/components"/{_buttons.scss,_card.scss,_icons.scss,_icons.scss} ;
+touch "$PWD/../www/$filePath/$projectName/scss/components"/{_buttons.scss,_card.scss,_icons.scss,_icons.scss} ;
 #scss/layouts
-touch "$PWD/$filePath/$projectName/scss/layouts"/{_container.scss,_footer.scss,_header.scss,_index.scss} ;
+touch "$PWD/../www/$filePath/$projectName/scss/layouts"/{_container.scss,_footer.scss,_header.scss,_index.scss} ;
 #scss/pages
-touch "$PWD/$filePath/$projectName/scss/pages"/{_about.scss,_home.scss,_index.scss} ;
+touch "$PWD/../www/$filePath/$projectName/scss/pages"/{_about.scss,_home.scss,_index.scss} ;
 #scss.utilities
-touch "$PWD/$filePath/$projectName/scss/utilities"/{_accessibility.scss,_alignment.scss,_display.scss,_flexbox.scss,_grid.scss,_index.scss,_spacing.scss,_visibility.scss} ;
+touch "$PWD/../www/$filePath/$projectName/scss/utilities"/{_accessibility.scss,_alignment.scss,_display.scss,_flexbox.scss,_grid.scss,_index.scss,_spacing.scss,_visibility.scss} ;
 
 
 tree
